2a51f6ee555d743a5877cfde80770de6f48974e1
customercounter 1 <huangcar@students.cs.ubc.ca>
2022-03-06 10:08:53 -0800

    added some more stuff

--- ECE569/frontend/src/Recipe.js
+++ ECE569/frontend/src/Recipe.js
@@ -1,11 +1,66 @@
 import React,{Component} from 'react';
 
+import {Button,ButtonToolbar} from 'react-bootstrap';
+
 export class Recipe extends Component{
 
+    constructor(props){
+        super(props);
+        this.state={recipes:[]}
+    }
+
+    refreshList(){ 
+        fetch()     //////////////////
+        .then(response=>Response.json())
+        .then(date=>{
+            this.setState({recipes: data});
+        })
+    }
+
+    componentDidMount(){
+        this.refreshList();
+    }
+
+    componentDidUpdate(){
+        this.refreshList();
+    }
+
     render(){
-        return(<div className='mt-5 d-flex justify-content-left'>
-            This is the recipe page.
-        </div>
+        const {recipes}=this.state;
+        let UpdateRecipeList=()=>this.setState();
+        return(
+        <div>
+            <Table recipeName="mt-4" striped bordered hover size="med">
+            <thead>
+                <tr>RecipeName</tr>
+                <tr>RecipeIngredients</tr>
+                <tr>TimeRequired</tr>
+                <tr>Options</tr>
+            </thead>
+            <tbody>
+                {recipes.map(recipe=>
+                     <tr key={recipe.recipeID}>
+                        <td>{recipe.recipeName}</td>
+                        <td>{recipe.recipeIngredients}</td>
+                        <td>{recipe.TimeRequired}</td>
+                        <td>Update Recipes</td>
+                    </tr>)}
+            </tbody>
+
+        </Table>
+
+        <ButtonToolbar>
+            <Button variant='primary'
+            onClick={()=>this.setState()}>
+            Add Food</Button>
+
+            <AddFoodModal show ={this.state.addModalShow}
+            onHide={addModalClose}>
+            </AddFoodModal>
+         </ButtonToolbar>
+      </div>
         )
     }
 }
+
+
