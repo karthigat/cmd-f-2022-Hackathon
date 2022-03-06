a0b624c1f460ccc0abbbdbd1ce933bc3845558a3
karthiga <karthigathangavel.2011@gmail.com>
2022-03-06 10:16:06 -0800

    fetching food in recipes

--- ECE569/frontend/src/Recipe.js
+++ ECE569/frontend/src/Recipe.js
@@ -2,9 +2,34 @@ import React,{Component} from 'react';
 
 export class Recipe extends Component{
 
+    constructor(props){
+        super(props);
+        this.state={items:[], AddFoodModal:false}
+    }
+
+    componentDidUpdate(){
+        this.getFoodItems();
+    }
+
+    getFoodItems(){
+        fetch("http://localhost:8000/api/get_recipes")
+        .then(response=>Rrsponse.json())
+        .then(data=>{
+            this.setState({items: data});
+        })
+
+    }
+
     render(){
         return(<div className='mt-5 d-flex justify-content-left'>
-            This is the recipe page.
+            <li>
+                {items.map((item,i) => { return(<li>{item.foodname}
+                <div class="checkbox checkbox-circle checkbox-color-scheme">
+                        <label class="checkbox-checked">
+                            <input type="checkbox" value={item.foodname} checked={item.isChecked} onChange={this.onAddingItem(i)}/>
+                        </label>
+                    </div></li>)})}
+            </li>
         </div>
         )
     }
