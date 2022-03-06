659e807274215a9dc05575769356e72a55ced6cc
customercounter 1 <huangcar@students.cs.ubc.ca>
2022-03-06 00:31:41 -0800

    Added some stuff

--- ECE569/homepage/models.py
+++ ECE569/homepage/models.py
@@ -5,4 +5,16 @@ from django.conf import settings
 import requests
 import json
 # from homepage.get_location import FindCity
-# Create your models here.
+
+class Foods(models.Model):
+    FoodID = models.AutoField(primary_key = True)
+    FoodName = models.CharField(max_length = 150)
+    FoodType = models.CharField(max_length = 100)
+    FoodDate = models.DateField()
+
+class Recipes(models.Model):
+    RecipeID = models.AutoField(primary_key = True)
+    RecipeName = models.CharField(max_length = 200)
+    RecipeType = models.CharField(max_length = 200)
+    
+
--- ECE569/homepage/urls.py
+++ ECE569/homepage/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
 from . import views
 from django.conf.urls import include, url
-# from .views import AWSUploadView
+ # from .views import AWSUploadView
+
+urlpatterns=[
+    url(r'^food$', views.foodApi),
+    url(r'^food/([0-9]+)$', views.foodApi),
+
+    url(r'^recipe$', views.foodApi),
+    url(r'^recipe/([0-9]+)$', views.foodApi),
 
 
-
+    url(r'^Food/SaveFile$', views.SaveFile)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
